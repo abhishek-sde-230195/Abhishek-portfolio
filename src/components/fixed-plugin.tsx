babafc24b9f8887f6852d00e0a27dd7cce75d831
230195 <sharma.abhi554@gmail.com>
2024-02-20 19:56:23 +0100

    Update fixed-plugin.tsx

--- src/components/fixed-plugin.tsx
+++ src/components/fixed-plugin.tsx
@@ -8,6 +8,7 @@ import {
 export function FixedPlugin() {
   return (
       <Button
+        placeholder = ""
         color="white"
         size="sm"
         className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50"
