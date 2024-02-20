5f5beacf45f9ea77db04bcd785702d27922e885d
230195 <sharma.abhi554@gmail.com>
2024-02-20 19:33:59 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
 // components
-import { Navbar, Footer } from "@/components";
+import { Footer } from "@/components";
 
 // sections
 import Hero from "./hero";
-import Clients from "./companies";
+import Companies from "./companies";
 import Skills from "./skills";
 import Projects from "./projects";
 import Resume from "./resume";
-import Testimonial from "./testimonial";
-import PopularClients from "./popular-clients";
 import ContactForm from "./contact-form";
 
 export default function Portfolio() {
   return (
     <>
       <Hero />
-      <Clients />
+      <Companies />
       <Skills />
       <Projects />
       <Resume />
